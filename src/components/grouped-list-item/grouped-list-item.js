2526c7b883ecfcf6afe1133fdf84650ffb8922e8
N Nemakhavhani <nbanzyme@gmail.com>
2019-09-15 22:43:03 +0200

    fix idx issue

--- src/components/grouped-list-item/grouped-list-item.js
+++ src/components/grouped-list-item/grouped-list-item.js
@@ -1,39 +1,38 @@
-import React, {useState,useEffect} from "react";
-import './grouped-list-item.css';
+import React, { useState, useEffect } from "react";
+import "./grouped-list-item.css";
 
-const GroupedListItem = ({mealOptions, period}) => {
-
-  const [mealsList, setmealsList] = useState(mealOptions? mealOptions:[]);
+const GroupedListItem = ({ mealOptions, period }) => {
+  const [mealsList, setmealsList] = useState(mealOptions ? mealOptions : []);
   const [mealOptionIdx, setMealOptionIdx] = useState(0);
   const [visibleMealOption, setVisibleMealOption] = useState(mealsList[0]);
 
-  const viewNextOption = () =>{
+  const viewNextOption = () => {
     let newIdx = mealOptionIdx + 1;
-    if(newIdx >= mealOptions.length){
+    
+    if (newIdx >= mealOptions.length) {
       newIdx = 0;
     }
     setMealOptionIdx(newIdx);
-    setVisibleMealOption(mealsList[mealOptionIdx]);
+    setVisibleMealOption(mealsList[newIdx]);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     setVisibleMealOption(mealOptions[0]);
     setmealsList(mealOptions);
-  }, [mealOptions])
+  }, [mealOptions]);
 
   return (
     <div className="content-wrapper">
       <section className="list-item">
-        <div className="list-item-bullet">
-          {period}
-          {/* <i className="fa fa-ellipsis-v"></i> */}
-        </div>
+        <div className="list-item-bullet">{period}</div>
 
         <div className="list-item-content">
-        { visibleMealOption? visibleMealOption.shortName:'' }
-        <p className="descrList">
-          <span>{ visibleMealOption? visibleMealOption.description:'' }</span>
-        </p>
+          {visibleMealOption ? visibleMealOption.shortName : ""}
+          <p className="descrList">
+            <span>
+              {visibleMealOption ? visibleMealOption.description : ""}
+            </span>
+          </p>
         </div>
         <div className="list-item-action-btn" onClick={viewNextOption}>
           <i className="fa fa-chevron-right"></i>
