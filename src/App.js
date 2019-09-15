e53d3a0f2f9e3db6d8d5a41e02c56b1784f3c47d
N Nemakhavhani <nbanzyme@gmail.com>
2019-09-15 22:34:29 +0200

    update default routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import DailyViewPage from './pages/daily-view-page/daily-view-page';
-import  AddMealPage from "./pages/add-meal/add-meal.page";
-
-
-function NavGrid(props) {
-  const onaction = ()=>{
-    console.log("History:  ", props.history)
-    props.history.push('/events');
-  }
-  return (
-    <article className="navGrid">
-      <section onClick={onaction} className="events">
-        <Link to="/events">A</Link>
-      </section>
-      <section className="grid-cell logs">B</section>
-
-      <section className="grid-cell venues">C</section>
-
-      <section className="grid-cell artists">E</section>
-
-      <section className="grid-cell logs">E</section>
-    </article>
-  );
-}
+import DailyViewPage from "./pages/daily-view-page/daily-view-page";
+import AddMealPage from "./pages/add-meal/add-meal.page";
 
 function App() {
-
   return (
     <Router className="content-container">
       <header className="App-header">
         <h4>Chow</h4>
       </header>
       <main>
-        <Route path="/" exact component={NavGrid} />
-        <Route path="/events" component={DailyViewPage} />
-        <Route path="/create" component={AddMealPage}/>
+        <Route path="/" exact component={DailyViewPage} />
+        <Route path="/create" component={AddMealPage} />
       </main>
       <footer>
         <span>
---
+++ src/pages/nav-grid/nav-grid.page.js
@@ -0,0 +1,24 @@
+import './nav-grid.page.css';
+
+function NavGrid(props) {
+  const onaction = () => {
+    console.log("History:  ", props.history);
+    props.history.push("/events");
+  };
+  return (
+    <article className="navGrid">
+      <section onClick={onaction} className="events">
+        <Link to="/events">A</Link>
+      </section>
+      <section className="grid-cell logs">B</section>
+
+      <section className="grid-cell venues">C</section>
+
+      <section className="grid-cell artists">E</section>
+
+      <section className="grid-cell logs">E</section>
+    </article>
+  );
+}
+
+export default NavGrid;
