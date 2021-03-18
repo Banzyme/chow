725df27b3bdc9d5953eae9424a42922e72a190fc
endeesa <endeesa@bbd.extra>
2021-03-18 08:03:14 +0200

    update main meal options model

--- src/models/mealOptionModel.js
+++ src/models/mealOptionModel.js
@@ -1,4 +1,36 @@
+function isNullOrEmpty(val){
+    return val == undefined || val == null || val == "";
+}
+
 export class MealOptionModel {
 
+    constructor({name, description, category, calories=0, isLocked=false, thumbnailURL=""}){
+        if(isNullOrEmpty(name)){
+            throw "Name is required"
+        }
+        this.id = "";
+        this.name = name; 
+        this.description = description;
+        this.category = category;
+        this.calories = calories;
+        this.isLocked= isLocked ;
+        this.thumbnailURL = thumbnailURL;
+        this.dateCreated = new Date().toString();
+        this.lastModifiedCreated = new Date().toString();
+    }
 
+    isNullOrEmpty(val){
+        return val == undefined || val == null || val == "";
+    }
+
+    loadJson({jsonData}){
+        this.id = jsonData?.id ?? this.id;
+        this.name = jsonData?.name ?? this.name;
+        this.category = jsonData?.category ?? this.category;
+        this.calories = jsonData?.calories ?? this.calories;
+        this.isLocked = jsonData?.isLocked ?? this.isLocked;
+        this.thumbnailURL = jsonData?.thumbnailURL ?? this.thumbnailURL;
+        this.dateCreated = jsonData?.dateCreated ?? this.dateCreated;
+        return  this;
+    }
 }
