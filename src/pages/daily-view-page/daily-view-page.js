6cd9edba87e80f5b97a86df3a871fb65ef1129f0
endeesa <endeesa@bbd.extra>
2021-03-19 16:38:50 +0200

    Add left menu drawer

--- src/pages/daily-view-page/daily-view-page.js
+++ src/pages/daily-view-page/daily-view-page.js
@@ -6,6 +6,37 @@ import { MealCard } from "./../../components/components";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import "./daily-view-page.css";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: "indigo"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 
 const filterMealsByCategory = ({ allMealsList, category }) => {
@@ -17,9 +48,16 @@ const filterMealsByCategory = ({ allMealsList, category }) => {
 }
 
 const DailyViewPage = props => {
+  const classes = useStyles();
   const [hasErrors, setHasErros] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   const [mealOptions, setMealOptions] = useState([]);
   const fetchUrl = `${AppSettings.mealsAPI.baseURL}/mealoptions`;
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  }
 
   useEffect(() => {
     console.debug("principal info", getUserInfo());
@@ -58,9 +96,9 @@ const DailyViewPage = props => {
 
   async function getUserInfo() {
     const response = await fetch("/.auth/me");
-    if(response?.status == 200 || response?.ok){
+    if (response?.status == 200 || response?.ok) {
       console.debug("User info retrieved!")
-    }else{
+    } else {
       console.warn("auth.me returned", response);
       return;
     }
@@ -77,8 +115,80 @@ const DailyViewPage = props => {
     props.history.push(`/details/${params.mealId}`);
   }
 
+  const handleLogin = () => {
+    // by deafult user will be redirected to home after login (i think...)
+    props.history.push('/login');
+    //
+  }
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleOpenLeftMenu = (event) => {
+    // setAnchorEl(event.currentTarget);
+    setDrawerOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+    props.history.push('logout');
+  };
+
   return (
     <>
+      <AppBar className={classes.root} position="static">
+        <Toolbar>
+          <IconButton onClick={handleOpenLeftMenu} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            {AppSettings.Apptitle}
+          </Typography>
+          <Button color="inherit" onClick={handleLogin}>Login</Button>
+        </Toolbar>
+      </AppBar>
+
+      <SwipeableDrawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer}
+      >
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+
+      </SwipeableDrawer>
+
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Preferences</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+
       <header className="page-header">
         <section>
           <h1>Daily meal plan</h1>
